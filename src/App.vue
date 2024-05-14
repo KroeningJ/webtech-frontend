a0bfc8b2238750ddbe2291d320d5e19d2f713a11
j <s0549399@htw-berlin.de>
2024-05-14 10:10:40 +0200

    Update: Added Comments

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
+  <!-- Haupt-Template, das die App-Komponente definiert -->
   <div id="app">
+    <!-- Eingebundene Navbar-Komponente mit übergebenen Navigationslinks -->
     <navbar :links="navLinks"></navbar>
+    <!-- Router-View, um die Inhalte der aktiven Route anzuzeigen -->
     <router-view/>
   </div>
 </template>
 
 <script>
+// Importiere die Navbar-Komponente aus dem angegebenen Pfad
 import Navbar from './components/Navbar'
 
 export default {
+  // Name der Hauptkomponente
   name: 'App',
+  // Registriere die Navbar-Komponente für die Verwendung in dieser App
   components: { Navbar },
+  // Data-Block für die Dateninitialisierung
   data () {
     return {
+      // Array von Navigationslinks, die der Navbar-Komponente übergeben werden
       navLinks: [
         {
           text: 'Home',
@@ -37,24 +45,27 @@ export default {
 </script>
 
 <style>
+/* Globale Stile für die gesamte App */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #365c24;
+  color: #365c24; /* Textfarbe für die gesamte App */
 }
 
+/* Stile für die Navbar innerhalb der App */
 #nav {
   padding: 30px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #365c24;
+  color: #365c24; /* Farbe der Navigationslinks */
 }
 
+/* Stile für den aktiven Router-Link in der Navbar */
 #nav a.router-link-exact-active {
-  color: #365c24;
+  color: #365c24; /* Farbe für den aktiven Navigationslink */
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,17 @@
-<!-- Navbar.vue -->
 <template>
+  <!-- Definiere das Navbar-Element mit Bootstrap-Klassen -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
+      <!-- Haupt-Brand-Link der Navbar -->
       <a class="navbar-brand" href="#">Notizplanner</a>
+      <!-- Toggle-Button für mobile Ansichten -->
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+      <!-- Container für die Navbar-Inhalte (wird bei Bedarf eingeklappt) -->
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
+          <!-- Iteriere über die übergebenen Navigationslinks und erstelle router-links -->
           <router-link v-for="(link, index) in links" :key="index" class="nav-link" :to="link.path">{{ link.text }}</router-link>
         </div>
       </div>
@@ -19,11 +23,11 @@
 export default {
   name: 'Nav-Bar',
   props: {
-    links: Array // Definiere den Prop-Typ für die Navigationslinks
+    links: Array // Definiere den Prop-Typ für die Navigationslinks als Array
   }
 }
 </script>
 
 <style scoped>
-/* Stil für die Navbar-Komponente */
+/* Hier können spezifische Stile für die Navbar-Komponente definiert werden */
 </style>
